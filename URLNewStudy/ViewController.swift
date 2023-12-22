d06fa5854ea52147de4e38a7831d7f35a240f6fe
AkiraRei <Akirarei@yandex.ru>
2023-12-22 18:30:42 +0300

    Initial Commit

---
+++ URLNewStudy/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  URLNewStudy
+//
+//  Created by Акира on 22.12.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
