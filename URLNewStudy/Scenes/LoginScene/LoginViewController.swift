f816088727a435f5c7c86fd1044ebc774fb662a9
AkiraRei <Akirarei@yandex.ru>
2024-01-06 18:39:56 +0300

    Setup background for TF, borderStyle, cornerRadius, delete usless label 

--- URLNewStudy/Scenes/LoginScene/LoginViewController.swift
+++ URLNewStudy/Scenes/LoginScene/LoginViewController.swift
@@ -90,13 +90,25 @@ private extension LoginViewController {
     func setupLoginTF() {
         loginTF.placeholder = "Enter your login"
         loginTF.textAlignment = .center
-        loginTF.backgroundColor = .gray
-        loginTF.alpha = 0.8
+        
+        loginTF.backgroundColor = .white
+        loginTF.alpha = 1
+        
+        loginTF.layer.borderColor = UIColor.systemBlue.cgColor
+        loginTF.layer.borderWidth = 2.0
+        loginTF.layer.cornerRadius = 10
     }
     
     func setupPasswordTF() {
         passwordTF.placeholder = "Enter your password"
         passwordTF.textAlignment = .center
+        
+        passwordTF.backgroundColor = .white
+        passwordTF.alpha = 1
+        
+        passwordTF.layer.borderColor = UIColor.systemBlue.cgColor
+        passwordTF.layer.borderWidth = 2.0
+        passwordTF.layer.cornerRadius = 10
         
     }
 }
@@ -122,21 +134,21 @@ private extension LoginViewController {
             loginLabel.topAnchor.constraint(equalTo: view.topAnchor, constant: 300),
             loginLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
             
-            loginTF.topAnchor.constraint(equalTo: loginLabel.topAnchor, constant: 45),
+            loginTF.topAnchor.constraint(equalTo: loginLabel.topAnchor, constant: 25),
             loginTF.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 100),
             loginTF.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -100),
             
-            passwordLabel.topAnchor.constraint(equalTo: loginTF.topAnchor, constant: 45),
-            passwordLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 100),
-            passwordLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -100),
+//            passwordLabel.topAnchor.constraint(equalTo: loginTF.topAnchor, constant: 45),
+//            passwordLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 100),
+//            passwordLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -100),
             
-            passwordTF.topAnchor.constraint(equalTo: passwordLabel.topAnchor, constant: 45),
+            passwordTF.topAnchor.constraint(equalTo: loginTF.topAnchor, constant: 45),
             passwordTF.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 100),
             passwordTF.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -100),
             
             loginButton.topAnchor.constraint(equalTo: passwordTF.topAnchor, constant: 70),
-            loginButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 130),
-            loginButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -130),
+            loginButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 100),
+            loginButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -100),
             
         ])
     }
