58a1f7c5d6cba4040ddded7b0d93ccba234e4d2b
AkiraRei <Akirarei@yandex.ru>
2024-01-06 13:33:17 +0300

    make starterVC

---
+++ URLNewStudy/LoginScene/LoginScene.swift
@@ -0,0 +1,17 @@
+//
+//  LoginScene.swift
+//  URLNewStudy
+//
+//  Created by Акира on 24.12.2023.
+//
+
+import Foundation
+import UIKit
+
+class LoginViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+}
