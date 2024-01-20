7e41b50ebb169ddb8b48598571f570c452e8133a
DoctorRei <akirarei@yandex.ru>
2024-01-20 19:03:03 +0300

    Заглушка для сеттингов

--- URLNewStudy/Scenes/SettingsScene/SettingsRouter.swift
+++ URLNewStudy/Scenes/SettingsScene/SettingsRouter.swift
@@ -6,3 +6,37 @@
 //
 
 import Foundation
+import UIKit
+
+protocol ISettingsRouter: IBaseRouting  {
+    
+}
+
+final class SettingsRouter {
+    
+    enum Target {
+        case loginScene
+        case mainScene
+    }
+    
+    let navigationController: UINavigationController
+    
+    init(navigationController: UINavigationController) {
+        self.navigationController = navigationController
+    }
+}
+
+//TODO: - Заглушка навигации
+
+extension SettingsRouter: ISettingsRouter {
+    func routeTo(scene: Any) {
+        guard let randomImageTarget = scene as? RandomImageRouter.Target else {return}
+        
+        switch randomImageTarget {
+        case .loginScene:
+            break
+        case .mainScene:
+            break
+        }
+    }
+}
