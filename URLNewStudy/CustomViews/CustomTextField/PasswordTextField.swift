e1eee6575e630dd6701af19bca681f4412998219
DoctorRei <akirarei@yandex.ru>
2024-02-19 16:09:10 +0300

    Причесал текстфилд

--- URLNewStudy/CustomViews/CustomTextField/PasswordTextField.swift
+++ URLNewStudy/CustomViews/CustomTextField/PasswordTextField.swift
@@ -9,12 +9,30 @@ import UIKit
 
 
 final class PasswordTextField: UITextField {
+    
+    //MARK: - Stored Property
+    
     var eyeButton = UIButton(type: .custom)
     let padding = UIEdgeInsets(top: 0, left: 20, bottom: 0, right: 20)
+    
+    //MARK: - Init
     
     init(placeholder: String) {
         super.init(frame: .zero)
         
+        setupTextField(placeholder: placeholder)
+        setupSecurityTextField()
+        setupEyeButton()
+    }
+    
+    @available (*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    //MARK: - Setup View
+    
+    func setupTextField(placeholder: String) {
         self.placeholder = placeholder
         self.textAlignment = .center
         self.backgroundColor = .white
@@ -23,14 +41,6 @@ final class PasswordTextField: UITextField {
         self.layer.borderWidth = 2.0
         self.layer.cornerRadius = 10
         self.textContentType = .oneTimeCode
-        
-        setupSecurityTextField()
-        setupEyeButton()
-    }
-    
-    @available (*, unavailable)
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
     }
     
     func setupSecurityTextField() {
@@ -52,6 +62,8 @@ final class PasswordTextField: UITextField {
         rightViewMode = .always
     }
     
+    //MARK: - Ovveride methods
+    
     override func textRect(forBounds bounds: CGRect) -> CGRect {
         let rightPadding = eyeButton.frame.width + padding.right / 2 - 10
         return bounds.inset(by: UIEdgeInsets(top: padding.top, left: padding.left, bottom: padding.bottom, right: rightPadding))
@@ -66,8 +78,9 @@ final class PasswordTextField: UITextField {
         let rightPadding = eyeButton.frame.width + padding.right / 2 - 10
         return bounds.inset(by: UIEdgeInsets(top: padding.top, left: padding.left, bottom: padding.bottom, right: rightPadding))
     }
-    
 }
+
+//MARK: - Action
 
 extension PasswordTextField {
     @objc func togglePasswordView(_ sender: Any) {
